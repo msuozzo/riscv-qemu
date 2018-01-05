c381568dccf1726d741b5dc361a9c07904753f02
Michael Clark <mjc@sifive.com>
2018-01-05 16:51:31 +1300

    RISC-V - Change disassembler register name arrays to const

--- disas/riscv.c
+++ disas/riscv.c
@@ -534,9 +534,9 @@ typedef struct {
 } rv_comp_data;
 
 typedef struct {
-    const char *name;
+    const char * const name;
     const rv_codec codec;
-    const char *format;
+    const char * const format;
     const rv_comp_data *pseudo;
     const int decomp_rv32;
     const int decomp_rv64;
@@ -545,20 +545,18 @@ typedef struct {
 
 /* register names */
 
-static const char *rv_ireg_name_sym[] = {
+static const char rv_ireg_name_sym[32][5] = {
     "zero", "ra",   "sp",   "gp",   "tp",   "t0",   "t1",   "t2",
     "s0",   "s1",   "a0",   "a1",   "a2",   "a3",   "a4",   "a5",
     "a6",   "a7",   "s2",   "s3",   "s4",   "s5",   "s6",   "s7",
     "s8",   "s9",   "s10",  "s11",  "t3",   "t4",   "t5",   "t6",
-    NULL
 };
 
-static const char *rv_freg_name_sym[] = {
+static const char rv_freg_name_sym[32][5] = {
     "ft0",  "ft1",  "ft2",  "ft3",  "ft4",  "ft5",  "ft6",  "ft7",
     "fs0",  "fs1",  "fa0",  "fa1",  "fa2",  "fa3",  "fa4",  "fa5",
     "fa6",  "fa7",  "fs2",  "fs3",  "fs4",  "fs5",  "fs6",  "fs7",
     "fs8",  "fs9",  "fs10", "fs11", "ft8",  "ft9",  "ft10", "ft11",
-    NULL
 };
 
 /* instruction formats */
